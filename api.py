c1fc8ffa459e105d747f25ce40cd0a959a721286
Aditya Upadhyay <86370383+Apollo930@users.noreply.github.com>
2025-03-10 15:51:59 +0000

    Fixed stuff

--- api.py
+++ api.py
@@ -53,6 +53,6 @@ async def delete_file(filename: str):
 async def listfiles():
     try:
         resp = vercel_blob.list()
-        return JSONResponse(content=resp, status_code=200)
+        return JSONResponse(content={"files": resp.get("blobs", [])}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": "Failed to list files", "details": str(e)}, status_code=400)
